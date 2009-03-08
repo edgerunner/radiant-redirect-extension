592691cd36eb5a38912bc4437ba52162ee68eed8
Mert Torun <mert@merttorun.com>
2009-03-08 08:57:28 +0200

    description + admin tabs

--- redirect_extension.rb
+++ redirect_extension.rb
@@ -2,9 +2,9 @@
 # require_dependency 'application'
 
 class RedirectExtension < Radiant::Extension
-  version "1.0"
-  description "Describe your extension here"
-  url "http://yourwebsite.com/redirect"
+  version "0.1"
+  description "Manages a list of 30x redirects"
+  url "http://github.com/edgerunner/radiant-redirect-extension"
   
   # define_routes do |map|
   #   map.namespace :admin, :member => { :remove => :get } do |admin|
@@ -13,11 +13,11 @@ class RedirectExtension < Radiant::Extension
   # end
   
   def activate
-    # admin.tabs.add "Redirect", "/admin/redirect", :after => "Layouts", :visibility => [:all]
+    admin.tabs.add "Redirect", "/admin/redirect", :after => "Layouts", :visibility => [:all]
   end
   
   def deactivate
-    # admin.tabs.remove "Redirect"
+    admin.tabs.remove "Redirect"
   end
   
 end
